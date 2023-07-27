9c30aca20e39b7ed81ddb9e8cb06ed433a58354e
osvegn <thomas.prudhomme@epitech.eu>
2023-07-28 00:00:49 +0200

    [ADD] more test (coverage 97%)

--- tests/test_parser.c
+++ tests/test_parser.c
@@ -20,7 +20,7 @@ Test(init_parser, create_two_options_parser)
 {
     init_parser();
     cr_assert_eq(add_option("-v", "--version", "version", ""), 0);
-    cr_assert_eq(add_option("-h", "--help", "help", ""), 0);
+    cr_assert_eq(add_option("-q", "--quit", "quit", ""), 0);
     cr_assert_eq(get_parser()->option_count, 2);
     del_parser();
 }
@@ -63,12 +63,12 @@ Test(parse_args, parser_one_arg_one_option)
 
 Test(parse_args, parser_one_arg_two_options)
 {
-    char *args[] = {"test", "--help"};
+    char *args[] = {"test", "--quit"};
 
     init_parser();
     if (add_option("-v", "--version", "version", "") < 0)
         return;
-    if (add_option("-h", "--help", "help", "") < 0)
+    if (add_option("-q", "--quit", "quit", "") < 0)
         return;
     parse_args(2, args);
     cr_assert_eq(get_parser()->options[0].found, false);
@@ -83,11 +83,52 @@ Test(parse_args, parser_one_arg_one_option_with_value)
     init_parser();
     if (add_option("-v", "--version", "version", "") < 0)
         return;
-    if (add_option("-h", "--help", "help", "") < 0)
+    if (add_option("-q", "--quit", "quit", "") < 0)
         return;
     parse_args(3, args);
     cr_assert_eq(get_parser()->options[0].found, true);
     cr_assert_eq(strcmp(get_parser()->options[0].value, "vers"), 0);
     cr_assert_eq(get_parser()->options[1].found, false);
+    del_parser();
+}
+
+Test(parse_args, create_parser_null_help)
+{
+    char *args[] = {"test", "--version", "vers"};
+
+    init_parser();
+    if (add_option("-v", "--version", "version", NULL) < 0)
+        return;
+    parse_args(3, args);
+    cr_assert_eq(get_parser()->options[0].found, true);
+    del_parser();
+}
+
+Test(parse_args, parser_one_arg_one_option_with_value_and_short_option)
+{
+    char *args[] = {"test", "-v", "vers"};
+
+    init_parser();
+    if (add_option("-v", "--version", "version", "") < 0)
+        return;
+    if (add_option("-q", "--quit", "quit", "") < 0)
+        return;
+    parse_args(3, args);
+    cr_assert_eq(get_parser()->options[0].found, true);
+    cr_assert_eq(strcmp(get_parser()->options[0].value, "vers"), 0);
+    cr_assert_eq(get_parser()->options[1].found, false);
+    del_parser();
+}
+
+Test(parse_args, parser_one_arg_one_option_with_value_using_equal)
+{
+    char *args[] = {"test", "-v=salut"};
+
+    init_parser();
+    if (add_option("-v", "--version", "version", "") < 0)
+        return;
+    parse_args(2, args);
+    cr_assert_eq(get_parser()->options[0].found, true);
+    cr_assert_eq(strcmp(get_parser()->options[0].value, "salut"), 0);
     del_parser();
 }
