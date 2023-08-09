098966d21fc4f6742de81615b70fa1d7c156dcd5
Thomas Prud'homme <thomas.prudhomme@epitech.eu>
2023-08-09 11:39:56 +0000

    [FIX] install gcovr on debian based

--- install_criterion.sh
+++ install_criterion.sh
@@ -10,16 +10,20 @@ yum=$(which yum)
 
 if [ $res -ne 0 ]; then
     echo "Criterion is not installed"
+    rm -rf submodules/Criterion
+    git submodule update --init --remote --force
     if [ -n "$dnf" ]; then
         sudo dnf install -y gcovr meson
     elif [ -n "$apt" ]; then
-        sudo apt-get install -y gcovr meson
+        python -m pip install gcovr meson
+        sudo apt-get install cmake -y
     elif [ -n "$yum" ]; then
         sudo yum install -y gcovr meson
     else
         echo "Error: no package manager found"
         exit 1
     fi
+    sudo ldconfig
     cd submodules/Criterion
     meson build
     ninja -C build
